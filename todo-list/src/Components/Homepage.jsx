2eb5c17bb83e68d0c9339089bbad9086a6f88675
bharathaladandi <bharathaladandi@gmail.com>
2023-09-08 02:22:14 +0530

    Basic UI created

---
+++ todo-list/src/Components/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const HomePage = () => {
+    
+
+    return (
+        <div>
+            <div>
+                <h1>Todo List</h1>
+                <input type="text"
+                 placeholder='Add Name' />
+                 <button>ADD</button>
+            </div>
+        </div>
+    )
+
+}
